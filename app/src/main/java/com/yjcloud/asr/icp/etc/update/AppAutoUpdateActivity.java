fe0d23c089639c3fb890bfb78fc6865999893471
ruanzz <ruanzz@yunjiacloud.com>
2022-11-24 09:51:11 +0800

    fix issue#自动升级

--- app/src/main/java/com/yjcloud/asr/icp/etc/update/AppAutoUpdateActivity.java
+++ app/src/main/java/com/yjcloud/asr/icp/etc/update/AppAutoUpdateActivity.java
@@ -11,7 +11,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.widget.Toast;
 
